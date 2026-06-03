a69df39a92d48f06ba3affe787fabb6f2eb301f4
jk <markjudah8@gmail.com>
2026-06-03 08:55:52 +0300

    Avocado updates

---
+++ upande_avocado/upande_avocado/doctype/crop_estimate/crop_estimate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, dev@upande.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Crop Estimate", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- upande_avocado/upande_avocado/doctype/crop_estimate_block/crop_estimate_block.py
+++ upande_avocado/upande_avocado/doctype/crop_estimate_block/crop_estimate_block.py
@@ -4,5 +4,5 @@
 from frappe.model.document import Document
 
 
-class PalletAllocationItem(Document):
+class CropEstimateBlock(Document):
 	pass
